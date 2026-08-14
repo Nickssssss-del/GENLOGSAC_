aa6ad6cfc3c417c58726c7fdb905ee741c7fc2b3
Nicole <delacruzchaveznicole8@gmail.com>
2026-08-14 16:23:34 -0500

    Commit inicial: estructura del proyecto backend

---
+++ src/main/java/com/tuempresa/repuestos_backend/RepuestosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tuempresa.repuestos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepuestosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuempresa/repuestos_backend/RepuestosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuempresa.repuestos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepuestosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
